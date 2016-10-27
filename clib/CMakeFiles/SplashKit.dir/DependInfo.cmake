b38159b3f62ef60600264c5359baa702250b42af
Andrew Cain <acain@swin.edu.au>
2016-10-27 16:49:35 +1100

    NEW: Include all source files to build on linux

---
+++ clib/CMakeFiles/SplashKit.dir/DependInfo.cmake
@@ -0,0 +1,101 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/civetweb.c" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/civetweb.c.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/sqlite3.c" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/sqlite3.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ELPP_THREAD_SAFE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/animations.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/animations.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/audio.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/audio.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/audio_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/audio_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/bundles.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/bundles.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/camera.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/camera.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/circle_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/circle_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/circle_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/circle_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/clipping.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/clipping.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/collisions.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/collisions.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/color.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/color.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/core_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/core_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/database.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/database.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/database_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/database_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/drawing_options.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/drawing_options.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/ellipse_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/ellipse_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/graphics.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/graphics.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/graphics_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/graphics_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/images.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/images.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/input.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/input.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/input_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/input_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/json.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/json.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/json_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/json_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/keyboard_input.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/keyboard_input.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/line_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/line_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/line_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/line_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/logging_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/logging_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/matrix_2d.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/matrix_2d.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/mouse_input.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/mouse_input.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/music.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/music.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/network_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/network_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/networking.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/networking.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/physics.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/physics.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/point_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/point_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/point_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/point_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/quad_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/quad_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/random.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/random.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/rectangle_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/rectangle_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/rectangle_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/rectangle_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/resources.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/resources.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/sk_clib.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/sk_clib.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/sound.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/sound.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/sprites.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/sprites.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/terminal.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/terminal.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/text.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/text.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/text_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/text_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/text_input.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/text_input.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/timers.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/timers.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/triangle_drawing.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/triangle_drawing.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/triangle_geometry.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/triangle_geometry.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/types.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/types.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/utility_functions.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/utility_functions.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/utils.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/utils.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/utils_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/utils_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/vector_2d.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/vector_2d.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/web_client.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/web_client.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/web_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/web_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/web_server.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/web_server.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/web_server_driver.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/web_server_driver.cpp.o"
+  "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/window_manager.cpp" "/Users/acain/Documents/Code/GitHub/splashkit/out/splashkit-linux/clib/CMakeFiles/SplashKit.dir/window_manager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ELPP_THREAD_SAFE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
