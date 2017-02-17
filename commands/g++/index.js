ab0974d03e3e402192b4446084dc96867a1478f7
Andrew Cain <macite@gmail.com>
2017-02-17 15:58:22 +1100

    FIX: Add build instructions

--- commands/g++/index.js
+++ commands/g++/index.js
@@ -2,20 +2,20 @@ const utils = require('../../utils')
 const home = process.env.HOME
 
 execute = function (argv, callback) {
-  const cppBuild = `${home}/.splashkit/commands/clang++`
+  const cppBuild = `${home}/.splashkit/commands/g++`
   const sklibs = {
-    static: `${home}/.splashkit/commands/clang++/lib`,
+    static: `${home}/.splashkit/commands/g++/lib`,
     dynamic: `${home}/.splashkit/lib`
   }
 
   const userArgs = utils.argsToString(argv['original_string'])
   const clangArgs = `-L${sklibs.dynamic} -lSplashKit -L${sklibs.static} -lSplashKitCpp -I ${cppBuild}/include -rpath $ORIGIN -rpath ${sklibs.dynamic} -rpath /usr/local/lib`
 
-  utils.runCommand(`clang++ ${clangArgs} ${userArgs}`, function (err, data) {
+  utils.runCommand(`g++ ${clangArgs} ${userArgs}`, function (err, data) {
       if (err) {
           callback(err)
       } else {
-          callback(null, 'Successfully compiled with clang++ 🎉 ')
+          callback(null, 'Successfully compiled with g++ 🎉 ')
       }
   })
 }
